b8b3745a9250475f95aeeab7d2966602ae1a3207
albert-dev <al96@hotmail.es>
2026-06-10 13:38:37 +0200

    refactor: "changed .js tests to .ts tests"

--- tests/lottery.test.ts
+++ tests/lottery.test.ts
@@ -1,9 +1,81 @@
 import { describe, test, expect } from 'vitest';
-import { lotteryResponseLogic } from '../src/scripts/lottery';
+import { lotteryLogic } from '../src/scripts/lottery';
+import { Messages } from '../src/types/messages';
 
-describe('lotteryResponseLogic', () => {
-    test('should return the succes message if user gets the number right', () => {
-       
-        expect().toBe();
+describe('Testing funció checkLotteryResult', () => {
+    test(`L'usuari ingressa el número 4 i el sistema genera un número aleatori diferent`, () => {
+
+        const userInput = 4;
+        const randomNum = 9;
+
+        const result = lotteryLogic(userInput, randomNum);
+
+        const message = Messages.tryAgainMessage(userInput, randomNum)
+
+        expect(result.result).toBe(message);
+    });
+});
+
+describe('Testing funció checkLotteryResult', () => {
+    test(`L'usuari ingressa el número 7 i el sistema genera el mateix número aleatori`, () => {
+
+        const userInput = 7;
+        const randomNum = 7;
+
+        const result = lotteryLogic(userInput, randomNum);
+
+        const message = Messages.successMessage(userInput, randomNum)
+
+        expect(result.result).toBe(message);
+    });
+});
+
+describe('Testing funció checkLotteryResult', () => {
+    test(`L'usuari ingressa un string i la app ha de mostrar un missatge d'error`, () => {
+
+        const userInput = NaN;
+        const randomNum = 5;
+
+        expect(() => lotteryLogic(userInput, randomNum)).toThrow(Messages.errorMessage());
+    });
+});
+
+describe('Testing funció checkLotteryResult', () => {
+    test(`L'usuari ingressa el número 20 i la app ha de mostrar un missatge d'error`, () => {
+
+        const userInput = 20;
+        const randomNum = 5;
+
+        expect(() => lotteryLogic(userInput, randomNum)).toThrow(Messages.errorMessage());
+    });
+});
+
+describe('Testing funció checkLotteryResult', () => {
+    test(`L'usuari ingressa el número 3 i el número 8 de forma seqüencial, l'historial ha de mostrar el missatge de les dues jugades`, () => {
+
+      const history = [];
+      
+      const userInput1 = 3;
+      const randomNum1 = 5;
+
+      const result1 = lotteryLogic(userInput1, randomNum1);
+
+      history.push(result1);
+
+      const message1 = Messages.tryAgainMessage(userInput1, randomNum1);
+
+      expect(history[0].result).toBe(message1);
+      
+      const userInput2 = 8;
+      const randomNum2 = 8;
+      
+      const result2 = lotteryLogic(userInput2, randomNum2);
+
+      history.push(result2);
+    
+      const message2 = Messages.successMessage(userInput2, randomNum2);
+      expect(history[1].result).toBe(message2);
+          
+      expect(history.length).toBe(2);
     });
 });
